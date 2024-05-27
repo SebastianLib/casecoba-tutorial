8cda3e01330509617608ffe3fc9eece48c9fa14c
SebastianLib <sebastianlib04@gmail.com>
2024-05-27 23:56:32 +0200

    created: upload page

---
+++ src/components/MaxWidthWrapper.tsx
@@ -0,0 +1,23 @@
+import { cn } from "@/lib/utils";
+import { ReactNode } from "react";
+
+const MaxWidthWrapper = ({
+  children,
+  className,
+}: {
+  children: ReactNode;
+  className?: string;
+}) => {
+  return (
+    <div
+      className={cn(
+        "h-full mx-auto w-full max-w-6xl px-2.5 md:px-20",
+        className
+      )}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default MaxWidthWrapper;
